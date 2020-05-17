ef2547e65ff556290adb1055d11bc3160f5c28dd
Maciej Kazimierski <maciej.kazimierski@icsec.pl>
2020-05-17 18:24:27 +0200

    cw8_cw9

---
+++ LinqConsoleApp/LinqSamples.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace LinqConsoleApp
+{
+    partial class LinqSamples
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+       #region Windows Form Designer generated code
+
+       /// <summary>
+       /// Required method for Designer support - do not modify
+       /// the contents of this method with the code editor.
+       /// </summary>
+       private void InitializeComponent()
+       {
+           this.Example1 = new System.Windows.Forms.Button();
+           this.Example2 = new System.Windows.Forms.Button();
+           this.Example3 = new System.Windows.Forms.Button();
+           this.Example4 = new System.Windows.Forms.Button();
+           this.Example5 = new System.Windows.Forms.Button();
+           this.Example6 = new System.Windows.Forms.Button();
+           this.Example7 = new System.Windows.Forms.Button();
+           this.Example8 = new System.Windows.Forms.Button();
+           this.Example9 = new System.Windows.Forms.Button();
+           this.Example10 = new System.Windows.Forms.Button();
+           this.Example11 = new System.Windows.Forms.Button();
+           this.Example12 = new System.Windows.Forms.Button();
+           this.ResultsList = new System.Windows.Forms.DataGridView();
+           this.ResultsTextBox = new System.Windows.Forms.TextBox();
+           ((System.ComponentModel.ISupportInitialize) (this.ResultsList)).BeginInit();
+           this.SuspendLayout();
+           // 
+           // Example1
+           // 
+           this.Example1.Location = new System.Drawing.Point(8, 385);
+           this.Example1.Name = "Example1";
+           this.Example1.Size = new System.Drawing.Size(77, 25);
+           this.Example1.TabIndex = 1;
+           this.Example1.Text = "Example1";
+           this.Example1.Click += new System.EventHandler(this.Example1Button_Click);
+           // 
+           // Example2
+           // 
+           this.Example2.Location = new System.Drawing.Point(91, 385);
+           this.Example2.Name = "Example2";
+           this.Example2.Size = new System.Drawing.Size(77, 25);
+           this.Example2.TabIndex = 2;
+           this.Example2.Text = "Example2";
+           this.Example2.Click += new System.EventHandler(this.Example2Button_Click);
+           // 
+           // Example3
+           // 
+           this.Example3.Location = new System.Drawing.Point(174, 385);
+           this.Example3.Name = "Example3";
+           this.Example3.Size = new System.Drawing.Size(77, 25);
+           this.Example3.TabIndex = 3;
+           this.Example3.Text = "Example3";
+           this.Example3.UseVisualStyleBackColor = true;
+           this.Example3.Click += new System.EventHandler(this.Example3Button_Click);
+           // 
+           // Example4
+           // 
+           this.Example4.Location = new System.Drawing.Point(257, 385);
+           this.Example4.Name = "Example4";
+           this.Example4.Size = new System.Drawing.Size(77, 25);
+           this.Example4.TabIndex = 4;
+           this.Example4.Text = "Example4";
+           this.Example4.Click += new System.EventHandler(this.Example4Button_Click);
+           // 
+           // Example5
+           // 
+           this.Example5.Location = new System.Drawing.Point(340, 385);
+           this.Example5.Name = "Example5";
+           this.Example5.Size = new System.Drawing.Size(77, 25);
+           this.Example5.TabIndex = 5;
+           this.Example5.Text = "Example5";
+           this.Example5.Click += new System.EventHandler(this.Example5Button_Click);
+           // 
+           // Example6
+           // 
+           this.Example6.Location = new System.Drawing.Point(423, 385);
+           this.Example6.Name = "Example6";
+           this.Example6.Size = new System.Drawing.Size(77, 25);
+           this.Example6.TabIndex = 6;
+           this.Example6.Text = "Example6";
+           this.Example6.Click += new System.EventHandler(this.Example6Button_Click);
+           // 
+           // Example7
+           // 
+           this.Example7.Location = new System.Drawing.Point(8, 416);
+           this.Example7.Name = "Example7";
+           this.Example7.Size = new System.Drawing.Size(77, 25);
+           this.Example7.TabIndex = 7;
+           this.Example7.Text = "Example7";
+           this.Example7.Click += new System.EventHandler(this.Example7Button_Click);
+           // 
+           // Example8
+           // 
+           this.Example8.Location = new System.Drawing.Point(91, 416);
+           this.Example8.Name = "Example8";
+           this.Example8.Size = new System.Drawing.Size(77, 25);
+           this.Example8.TabIndex = 8;
+           this.Example8.Text = "Przykła8";
+           this.Example8.Click += new System.EventHandler(this.Example8Button_Click);
+           // 
+           // Example9
+           // 
+           this.Example9.Location = new System.Drawing.Point(174, 416);
+           this.Example9.Name = "Example9";
+           this.Example9.Size = new System.Drawing.Size(77, 25);
+           this.Example9.TabIndex = 9;
+           this.Example9.Text = "Example9";
+           this.Example9.Click += new System.EventHandler(this.Example9Button_Click);
+           // 
+           // Example10
+           // 
+           this.Example10.Location = new System.Drawing.Point(257, 416);
+           this.Example10.Name = "Example10";
+           this.Example10.Size = new System.Drawing.Size(77, 25);
+           this.Example10.TabIndex = 10;
+           this.Example10.Text = "Example10";
+           this.Example10.Click += new System.EventHandler(this.Example10Button_Click);
+           // 
+           // Example11
+           // 
+           this.Example11.Location = new System.Drawing.Point(340, 416);
+           this.Example11.Name = "Example11";
+           this.Example11.Size = new System.Drawing.Size(77, 25);
+           this.Example11.TabIndex = 11;
+           this.Example11.Text = "Example11";
+           this.Example11.Click += new System.EventHandler(this.Example11Button_Click);
+           // 
+           // Example12
+           // 
+           this.Example12.Location = new System.Drawing.Point(423, 416);
+           this.Example12.Name = "Example12";
+           this.Example12.Size = new System.Drawing.Size(77, 25);
+           this.Example12.TabIndex = 12;
+           this.Example12.Text = "Example12";
+           this.Example12.Click += new System.EventHandler(this.Example12Button_Click);
+           // 
+           // Results
+           // 
+           this.ResultsList.ColumnHeadersHeightSizeMode = System.Windows.Forms
+               .DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+           this.ResultsList.Location = new System.Drawing.Point(10, 12);
+           this.ResultsList.Name = "ResultsList";
+           this.ResultsList.Size = new System.Drawing.Size(778, 341);
+           this.ResultsList.TabIndex = 14;
+           // 
+           // textBox1
+           // 
+           this.ResultsTextBox.Location = new System.Drawing.Point(10, 359);
+           this.ResultsTextBox.Name = "ResultsTextBox";
+           this.ResultsTextBox.Size = new System.Drawing.Size(778, 20);
+           this.ResultsTextBox.TabIndex = 15;
+           // 
+           // LinqSamples
+           // 
+           this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+           this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+           this.ClientSize = new System.Drawing.Size(800, 450);
+           this.Controls.Add(this.ResultsTextBox);
+           this.Controls.Add(this.ResultsList);
+           this.Controls.Add(this.Example1);
+           this.Controls.Add(this.Example2);
+           this.Controls.Add(this.Example3);
+           this.Controls.Add(this.Example4);
+           this.Controls.Add(this.Example5);
+           this.Controls.Add(this.Example6);
+           this.Controls.Add(this.Example7);
+           this.Controls.Add(this.Example8);
+           this.Controls.Add(this.Example9);
+           this.Controls.Add(this.Example10);
+           this.Controls.Add(this.Example11);
+           this.Controls.Add(this.Example12);
+           this.Name = "LinqSamples";
+           this.Text = "Display";
+           ((System.ComponentModel.ISupportInitialize) (this.ResultsList)).EndInit();
+           this.ResumeLayout(false);
+           this.PerformLayout();
+       }
+
+       private System.Windows.Forms.Button Example1;
+       private System.Windows.Forms.Button Example10;
+       private System.Windows.Forms.Button Example11;
+       private System.Windows.Forms.Button Example12;
+       private System.Windows.Forms.Button Example2;
+       private System.Windows.Forms.Button Example3;
+       private System.Windows.Forms.Button Example4;
+       private System.Windows.Forms.Button Example5;
+       private System.Windows.Forms.Button Example6;
+       private System.Windows.Forms.Button Example7;
+       private System.Windows.Forms.Button Example8;
+       private System.Windows.Forms.Button Example9;
+       private System.Windows.Forms.DataGridView ResultsList;
+       private System.Windows.Forms.TextBox ResultsTextBox;
+
+       #endregion
+    }
+}
